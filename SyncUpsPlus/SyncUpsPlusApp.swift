6540eec7f5e0ecc5430a7dd30c838d7f8f0cd78f
Sasha Jaroshevskii 🦄 <72662383+jaroshevskii@users.noreply.github.com>
2025-08-27 00:32:58 +0300

    Initial Commit

---
+++ SyncUpsPlus/SyncUpsPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncUpsPlusApp.swift
+//  SyncUpsPlus
+//
+//  Created by Sasha Jaroshevskii on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SyncUpsPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SyncUpsPlusTests/SyncUpsPlusTests.swift
@@ -0,0 +1,17 @@
+//
+//  SyncUpsPlusTests.swift
+//  SyncUpsPlusTests
+//
+//  Created by Sasha Jaroshevskii on 8/27/25.
+//
+
+import Testing
+@testable import SyncUpsPlus
+
+struct SyncUpsPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
