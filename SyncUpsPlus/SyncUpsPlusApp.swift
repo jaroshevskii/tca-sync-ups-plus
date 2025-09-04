246ec2d2bf29d53fbc86d1f78ddc5012eaa2054b
Sasha Jaroshevskii 🦄 <72662383+jaroshevskii@users.noreply.github.com>
2025-09-04 23:53:40 +0300

    Writing your first test

--- SyncUpsPlus/SyncUpsPlusApp.swift
+++ SyncUpsPlus/SyncUpsPlusApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct SyncUpsPlusApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
