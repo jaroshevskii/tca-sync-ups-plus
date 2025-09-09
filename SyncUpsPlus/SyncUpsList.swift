52faf00a959bfe12ca7aa2b279eb0ba0fc720250
Sasha Jaroshevskii 🦄 <72662383+jaroshevskii@users.noreply.github.com>
2025-09-09 16:23:47 +0300

    Integrating the feature domains together

--- SyncUpsPlus/SyncUpsList.swift
+++ SyncUpsPlus/SyncUpsList.swift
@@ -12,11 +12,13 @@ import SwiftUI
 struct SyncUpsList {
   @ObservableState
   struct State: Equatable {
+    @Presents var addSyncUp: SyncUpForm.State?
     var syncUps: IdentifiedArrayOf<SyncUp> = []
   }
   
   enum Action {
     case addSyncUpButtonTapped
+    case addSyncUp(PresentationAction<SyncUpForm.Action>)
     case onDelete(IndexSet)
     case syncUpTapped(id: SyncUp.ID)
   }
@@ -25,15 +27,22 @@ struct SyncUpsList {
     Reduce { state, action in
       switch action {
       case .addSyncUpButtonTapped:
+        state.addSyncUp = SyncUpForm.State(syncUp: SyncUp(id: SyncUp.ID()))
+        return .none
+        
+      case .addSyncUp:
         return .none
         
       case let .onDelete(indexSet):
         state.syncUps.remove(atOffsets: indexSet)
         return .none
         
-      case let .syncUpTapped(id):
+      case .syncUpTapped:
         return .none
       }
+    }
+    .ifLet(\.$addSyncUp, action: \.addSyncUp) {
+      SyncUpForm()
     }
   }
 }
