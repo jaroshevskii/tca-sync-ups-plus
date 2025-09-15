a967a2ddfea04fa4f699fe0be560df811753bf68
Sasha Jaroshevskii 🦄 <72662383+jaroshevskii@users.noreply.github.com>
2025-09-15 18:07:12 +0300

    Non-exhaustive testing

--- SyncUpsPlusTests/SyncUpsListTest.swift
+++ SyncUpsPlusTests/SyncUpsListTest.swift
@@ -14,7 +14,33 @@ import Foundation
 @MainActor
 struct SyncUpsListTest {
   @Test
-  func addSyncup() async {
+  func addSyncUpNonExhaustive() async {
+    let store = TestStore(initialState: SyncUpsList.State()) {
+      SyncUpsList()
+    } withDependencies: {
+      $0.uuid = .incrementing
+    }
+    store.exhaustivity = .off(showSkippedAssertions: true)
+    
+    await store.send(.addSyncUpButtonTapped)
+    
+    let editedSyncUp = SyncUp(
+      id: SyncUp.ID(0),
+      attendees: [
+        Attendee(id: Attendee.ID(), name: "Blob"),
+        Attendee(id: Attendee.ID(), name: "Blob Jr."),
+      ],
+      title: "Point-Free morning sync"
+    )
+    await store.send(\.addSyncUp.binding.syncUp, editedSyncUp)
+    
+    await store.send(.confirmAddButtonTapped) {
+      $0.syncUps = [editedSyncUp]
+    }
+  }
+  
+  @Test
+  func addSyncUp() async {
     let store = TestStore(initialState: SyncUpsList.State()) {
       SyncUpsList()
     } withDependencies: {
