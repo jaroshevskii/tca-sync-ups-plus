4f99401366a7b39b6d09a0166da5f8c2aa05498e
Sasha Jaroshevskii 🦄 <72662383+jaroshevskii@users.noreply.github.com>
2025-09-16 20:05:22 +0300

    Create the sync-up detail

---
+++ SyncUpsPlus/SyncUpDetail.swift
@@ -0,0 +1,121 @@
+//
+//  SyncUpDetail.swift
+//  SyncUpsPlus
+//
+//  Created by Sasha Jaroshevskii on 9/16/25.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct SyncUpDetail {
+  @ObservableState
+  struct State: Equatable {
+    @Shared var syncUp: SyncUp
+  }
+  
+  enum Action {
+    case deleteButtonTapped
+    case editButtonTapped
+  }
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .deleteButtonTapped:
+        return .none
+        
+      case .editButtonTapped:
+        return .none
+      }
+    }
+  }
+}
+
+struct SyncUpDetailView: View {
+  let store: StoreOf<SyncUpDetail>
+  
+  var body: some View {
+    Form {
+      Section {
+        NavigationLink {
+        } label: {
+          Label("Start Meeting", systemImage: "timer")
+            .font(.headline)
+            .foregroundColor(.accentColor)
+        }
+        HStack {
+          Label("Length", systemImage: "clock")
+          Spacer()
+          Text(store.syncUp.duration.formatted(.units()))
+        }
+
+        HStack {
+          Label("Theme", systemImage: "paintpalette")
+          Spacer()
+          Text(store.syncUp.theme.name)
+            .padding(4)
+            .foregroundColor(store.syncUp.theme.accentColor)
+            .background(store.syncUp.theme.mainColor)
+            .cornerRadius(4)
+        }
+      } header: {
+        Text("Sync-up Info")
+      }
+
+      if !store.syncUp.meetings.isEmpty {
+        Section {
+          ForEach(store.syncUp.meetings) { meeting in
+            Button {
+            } label: {
+              HStack {
+                Image(systemName: "calendar")
+                Text(meeting.date, style: .date)
+                Text(meeting.date, style: .time)
+              }
+            }
+          }
+        } header: {
+          Text("Past meetings")
+        }
+      }
+
+      Section {
+        ForEach(store.syncUp.attendees) { attendee in
+          Label(attendee.name, systemImage: "person")
+        }
+      } header: {
+        Text("Attendees")
+      }
+
+      Section {
+        Button("Delete") {
+          store.send(.deleteButtonTapped)
+        }
+        .foregroundColor(.red)
+        .frame(maxWidth: .infinity)
+      }
+    }
+    .navigationTitle(Text(store.syncUp.title))
+    .toolbar {
+      Button("Edit") {
+        store.send(.editButtonTapped)
+      }
+    }
+  }
+}
+
+#Preview {
+  NavigationStack {
+    SyncUpDetailView(
+      store: Store(
+        initialState: SyncUpDetail.State(
+          syncUp: Shared(value: .mock)
+        )
+      ) {
+        SyncUpDetail()
+      }
+    )
+  }
+}
