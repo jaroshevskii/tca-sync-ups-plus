8fae068e2861c7f226e12a855b7db4e10c6e14a1
Sasha Jaroshevskii 🦄 <72662383+jaroshevskii@users.noreply.github.com>
2025-09-17 14:47:25 +0300

    Creating the app view

--- SyncUpsPlus/App.swift
+++ SyncUpsPlus/App.swift
@@ -45,6 +45,37 @@ struct AppFeature {
 
 extension AppFeature.Path.State: Equatable {}
 
+struct AppView: View {
+  @Bindable var store: StoreOf<AppFeature>
+  
+  var body: some View {
+    NavigationStack(
+      path: $store.scope(state: \.path, action: \.path)
+    ) {
+      SyncUpsListView(
+        store: store.scope(state: \.syncUpsList, action: \.syncUpsList)
+      )
+    } destination: { store in
+      switch store.case {
+      case let .detail(detailStore):
+        SyncUpDetailView(store: detailStore)
+      }
+    }
+  }
+}
+
+#Preview {
+  AppView(
+    store: Store(
+      initialState: AppFeature.State(
+        syncUpsList: SyncUpsList.State()
+      )
+    ) {
+      AppFeature()
+    }
+  )
+}
+
 @main
 struct SyncUpsPlusApp: App {
   @MainActor
